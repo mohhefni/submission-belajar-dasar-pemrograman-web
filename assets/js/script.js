f5e19ca98a8249ce80025c30eef6610fadd87484
MohHefni05 <49226685+MohHefni05@users.noreply.github.com>
2023-09-22 09:02:44 -0700

    no

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,46 @@
-const $arrow = document.querySelector('.arrow');
+// arrow animation iteration
+const arrow = document.querySelector('.arrow');
 
-$arrow.animate([{ top: '0' }, { top: '10px' }, { top: '0' }], {
+arrow.animate([{ top: '0' }, { top: '10px' }, { top: '0' }], {
   duration: 700,
   iterations: Infinity,
 });
+
+// dark mode
+const buttonIcon = document.getElementById('button-icon');
+const icon = document.getElementById('icon');
+
+buttonIcon.onclick = function () {
+  document.body.classList.toggle('dark-theme');
+  if (document.body.classList.contains('dark-theme')) {
+    icon.className = 'fa-regular fa-sun';
+  } else {
+    icon.className = 'fa-regular fa-moon';
+  }
+};
+
+// nav active
+const sectionHome = document.getElementById('home');
+const sectionBlog = document.getElementById('blog');
+const sectionProject = document.getElementById('project');
+const sectionAbout = document.getElementById('about');
+
+const sections = [sectionHome, sectionBlog, sectionProject, sectionAbout];
+const navLi = document.querySelectorAll('nav .list-item li a');
+
+window.onscroll = () => {
+  var current = '';
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute('id');
+      navLi.forEach((li) => {
+        li.classList.remove('active');
+        if (li.classList.contains(current)) {
+          li.classList.add('active');
+        }
+      });
+    }
+  });
+};
